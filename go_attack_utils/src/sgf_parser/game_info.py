f914fef0c1a1d37aba576c85bd4316a7720c5bb3
Tom Tseng <tom@alignmentfund.org>
2022-12-29 14:31:25 -0800

    make win_score None if resign

--- go_attack_utils/src/sgf_parser/game_info.py
+++ go_attack_utils/src/sgf_parser/game_info.py
@@ -198,6 +198,7 @@ def parse_game_str_to_dict(
     )
     victim_visits = {"b": b_visits, "w": w_visits}[victim_color]
     adv_visits = {"b": b_visits, "w": w_visits}[adv_color]
+    adv_komi = komi * {"w": 1, "b": -1}[adv_color]
     if win_color is None:
         adv_minus_victim_score = 0
     else:
@@ -209,17 +210,19 @@ def parse_game_str_to_dict(
         )
         if win_score_str == "R":
             # Resignation
-            win_score = 0
+            win_score = None
+            adv_minus_victim_score = None
+            adv_minus_victim_score_wo_komi = None
         else:
             win_score = float(win_score_str)
-        adv_minus_victim_score = win_score if adv_color == win_color else -win_score
+            adv_minus_victim_score = win_score if adv_color == win_color else -win_score
+            adv_minus_victim_score_wo_komi = adv_minus_victim_score - adv_komi
     adv_steps = (
         extract_re(r"\-s([0-9]+)\-", adv_name)
         or extract_re(r"t0\-s([0-9]+)\-", "/".join(parts[-3:]))
         or 0
     )
     adv_samples = extract_re(r"\-d([0-9]+)", adv_name) or 0
-    adv_komi = komi * {"w": 1, "b": -1}[adv_color]
 
     parsed_info = {
         "b_name": b_name,
@@ -251,7 +254,7 @@ def parse_game_str_to_dict(
         "komi": komi,
         "adv_komi": adv_komi,
         "adv_minus_victim_score": adv_minus_victim_score,
-        "adv_minus_victim_score_wo_komi": adv_minus_victim_score - adv_komi,
+        "adv_minus_victim_score_wo_komi": adv_minus_victim_score_wo_komi,
         # Other info
         "train_status": training,
         "board_size": board_size,
