088ece95766413a92a7062c972f9d7be6f23b399
UFO-101 <josephmiller101@gmail.com>
2023-02-14 02:58:11 +0000

    Fix pre-commit linting errors.

--- go_attack_utils/sample_training_games.py
+++ go_attack_utils/sample_training_games.py
@@ -7,15 +7,17 @@ import json
 random.seed(42)
 
 adv_pattern = re.compile("adv-s[0-9]+")
-victim_list = ['cp39h-v1', 'cp127h-v1', 'cp505h-v1', 'cp505h-v2048']
+victim_list = ["cp39h-v1", "cp127h-v1", "cp505h-v1", "cp505h-v2048"]
+
+
 def get_victim(input_text_line):
-    for victim in victim_list: # go through possible victims
+    for victim in victim_list:  # go through possible victims
         if victim in input_text_line:
             return victim
-        
+
     # if none found, something is wrong
-    raise Exception('Victim not identified')
-        
+    raise Exception("Victim not identified")
+
 
 all_files = glob.glob(
     "/nas/ucb/k8/go-attack/match/ttseng-hard-adv-checkpoint-sweep-497mil-221115/ttseng-hard-match-*/sgfs/*.sgfs"
@@ -32,11 +34,17 @@ for filename in all_files:
             victim = get_victim(line)
             if num_steps in results_dict:
                 if victim in results_dict[num_steps]:
-                    results_dict[num_steps][victim].append({"path": filename, "line": line_num + 1})
+                    results_dict[num_steps][victim].append(
+                        {"path": filename, "line": line_num + 1}
+                    )
                 else:
-                    results_dict[num_steps][victim] = [{"path": filename, "line": line_num + 1}]
+                    results_dict[num_steps][victim] = [
+                        {"path": filename, "line": line_num + 1}
+                    ]
             else:
-                results_dict[num_steps] = {victim : [{"path": filename, "line": line_num + 1}]}
+                results_dict[num_steps] = {
+                    victim: [{"path": filename, "line": line_num + 1}]
+                }
 
 
 # we will use this to find the nearest match (for which games are available) to the exact decile of training steps
@@ -57,31 +65,37 @@ for decile in range(1, 11):
     for victim in victim_list:
         assert victim in results_dict[nearest_match]
 
-        victim_name = victim.split('-v')[0]
+        victim_name = victim.split("-v")[0]
         victim_name_nocode = victim_name
-        if victim_name == 'cp505' or victim_name == 'cp505h':
-            victim_name = '<code>Latest</code>'
-            victim_name_nocode = 'Latest'
-        if victim_name[-1] == 'h':
+        if victim_name == "cp505" or victim_name == "cp505h":
+            victim_name = "<code>Latest</code>"
+            victim_name_nocode = "Latest"
+        if victim_name[-1] == "h":
             victim_name = victim_name[:-1]
-        victim_visits = victim.split('-v')[1]
-        if victim_visits == '1':
-            victim_visits = ', no search'
+        victim_visits = victim.split("-v")[1]
+        if victim_visits == "1":
+            victim_visits = ", no search"
         else:
-            victim_visits = f', {victim_visits} visits'
+            victim_visits = f", {victim_visits} visits"
 
         sample_games = random.sample(results_dict[nearest_match][victim], 5)
 
-        rounded_steps = str(nearest_match)[:-6] # round down to nearest million (remove last 6 digits)
+        rounded_steps = str(nearest_match)[
+            :-6
+        ]  # round down to nearest million (remove last 6 digits)
 
-        output_dict = {'title' : str(decile*10) + '% vs. ' + victim_name_nocode + victim_visits}
-        output_dict['dir_name'] = 'training_sample_games' + str(decile) + '_' + victim
-        output_dict['server'] = 'dqn.ist.berkeley.edu'
-        output_dict['path_comment'] = "Sampled using sample_training_games.py"
-        output_dict['paths_with_line_num'] = sample_games
-        output_dict['max_games'] = 5
-        output_dict['adversary'] = f"{rounded_steps} million training steps, 600 visits"
-        output_dict['victim'] = victim_name + victim_visits
-        output_dict['description'] = []
+        output_dict = {
+            "title": str(decile * 10) + "% vs. " + victim_name_nocode + victim_visits
+        }
+        output_dict["dir_name"] = "training_sample_games" + str(decile) + "_" + victim
+        output_dict["server"] = "dqn.ist.berkeley.edu"
+        output_dict["path_comment"] = "Sampled using sample_training_games.py"
+        output_dict["paths_with_line_num"] = sample_games
+        output_dict["max_games"] = 5
+        output_dict["adversary"] = f"{rounded_steps} million training steps, 600 visits"
+        output_dict["victim"] = victim_name + victim_visits
+        output_dict["description"] = []
 
-        print(json.dumps(output_dict) + ',') # add comma for easier copy/paste to content.ts - remove the last one when pasting!
+        print(
+            json.dumps(output_dict) + ","
+        )  # add comma for easier copy/paste to content.ts - remove the last one when pasting!
