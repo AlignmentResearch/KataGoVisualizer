88a4291750ac55c35beb711520eea3288faa131b
Kellin Pelrine <kpelrine@vae.ist.berkeley.edu>
2023-03-26 18:31:27 +0000

    retrigger tests 2

--- cyclic-position-benchmark/runcycletestsandplot.py
+++ cyclic-position-benchmark/runcycletestsandplot.py
@@ -284,7 +284,7 @@ if __name__ == "__main__":
         default="/go_attack/victim_models/",
     )
 
-    args = parser.parse_args()asdf
+    args = parser.parse_args()
 
     PLOTS_PATH = args.PLOTS_PATH
     SGFS_PATH = args.SGFS_PATH
