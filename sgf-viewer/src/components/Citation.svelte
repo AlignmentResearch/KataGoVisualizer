8ca8c51e95de3669ba9bb0b595ba1b7a494a7531
Tony Wang <tong.t.wang@gmail.com>
2023-07-18 19:45:03 -0600

    Update citation info

--- sgf-viewer/src/components/Citation.svelte
+++ sgf-viewer/src/components/Citation.svelte
@@ -9,9 +9,9 @@
     }
 </script>
 
-<hr>
+<hr />
 <div>
-    <h3>Cite this paper</h3>
+    <div class="citation">Citation Info</div>
     <code on:click={tdFocus}>
         <pre>
 {`@inproceedings{wang2023adversarial,
@@ -24,13 +24,31 @@
 </div>
 
 <style>
+    hr {
+        border: none;
+        height: 1px;
+        background: #000;
+        background: repeating-linear-gradient(
+            90deg,
+            #000,
+            #000 6px,
+            transparent 6px,
+            transparent 12px
+        );
+    }
+    .citation {
+        font-size: 22px;
+        font-weight: 300;
+        margin-top: 0.5em;
+        margin-bottom: 0em;
+    }
     div {
         display: flex;
         flex-direction: column;
         align-items: center;
     }
     pre {
-        width: 650px;
+        width: min(80vw, 800px);
         font-size: 13px;
     }
 </style>
