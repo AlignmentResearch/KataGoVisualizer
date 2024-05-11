aa38f2a72949864d75ce13a25901edc4dfefe647
Tom Tseng <tom.hm.tseng@gmail.com>
2024-05-11 14:27:47 -0700

    Update FAR logo

--- sgf-viewer/src/components/NavBar.svelte
+++ sgf-viewer/src/components/NavBar.svelte
@@ -25,8 +25,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
         <a class="navbar-brand" href="https://far.ai">
-            <img src="/images/far-logo.svg" alt="FAR logo" width="24" height="24" class="d-inline-block align-text-top">
-            FAR
+            <img src="/images/far-logo.svg" alt="FAR logo" width="65" height="25" class="d-inline-block align-text-top">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
