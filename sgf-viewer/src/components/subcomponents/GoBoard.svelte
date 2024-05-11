2a0e2610d8e2909b5657cf62a42cf66cb797eae7
Tom Tseng <tom.hm.tseng@gmail.com>
2024-05-11 11:42:57 -0700

    GoBoard: Fix responsiveness

--- sgf-viewer/src/components/subcomponents/GoBoard.svelte
+++ sgf-viewer/src/components/subcomponents/GoBoard.svelte
@@ -8,6 +8,8 @@
     export let sgfPath: string;
 
     let goBoardDiv: HTMLElement;
+    let wgoPlayer: WGo.BasicPlayer;
+    const LAST_MOVE_NUM = 10000;
 
     function releaseCanvas(canvas) {
         canvas.width = 1;
@@ -16,14 +18,12 @@
         ctx && ctx.clearRect(0, 0, 1, 1);
     }
 
-    $: if (goBoardDiv) {
+    $: if (goBoardDiv && !wgoPlayer) {
         [...goBoardDiv.getElementsByTagName("CANVAS")].forEach(releaseCanvas);
 
-        let wgoPlayer = new (<any>window).WGo.BasicPlayer(goBoardDiv, {
-            sgfFile: sgfPath,
+        wgoPlayer = new (<any>window).WGo.BasicPlayer(goBoardDiv, {
             layout: (<any>window).WGo.BasicPlayer.dynamicLayout,
             allowIllegalMoves: true,
-            move: 9999,
             enableWheel: false,
             board: {
                 // Options: NORMAL, PAINTED, REALISTIC, GLOW, SHELL, MONO, CR, LB, SQ, TR, MA, SL, SM, outline, mini
@@ -32,6 +32,9 @@
             },
         });
         wgoPlayers[dirName] = wgoPlayer;
+    }
+    $: if (wgoPlayer && sgfPath) {
+      wgoPlayer.loadSgfFromFile(sgfPath, LAST_MOVE_NUM);
     }
 </script>
 
