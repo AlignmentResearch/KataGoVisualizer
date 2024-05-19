4f07caeb15fc022667ba7661acd41179a232f07c
Adam Gleave <adam@gleave.me>
2024-05-19 14:40:51 +0100

    Revert vite.config changes -- minification enabled by default

--- sgf-viewer/vite.config.ts
+++ sgf-viewer/vite.config.ts
@@ -3,8 +3,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  build: {
-    minify: 'esbuild'
-  }
+  plugins: [svelte()]
 })
