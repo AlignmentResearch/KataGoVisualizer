AlignmentResearch/KataGoVisualizer

commit 8c7cdfb1e98505e28df86ec7f338b0a16ff66e3d

Go defense website (#99)
* squashed commit for rebasing

TableOfContents: Do not display if no contents

content: Update landing page with go-defense abstract

src: Update page title, title bar, citation

content: Add page on positional adv training

content: Add page on iterated adv training

content: Add page on ViT

title nit

content: Add per-iteration games

game_info.py: lint

src: Some refactoring of defense-specific content out

factor out defense-specific content

Move sgfs into subdir

Fix prepare_data bugs

Bring back attack SGFs

Update attack sgfs game_infos.csv

smaller svg

fix attack content

go-attack jump-to

horrible svelte misbehavior (bug?)

cleanup

lint

return to defense

Update sgf-viewer/index.html

Co-authored-by: Adam Gleave <adam@gleave.me>

Update sgf-viewer/src/defense/content.ts

Co-authored-by: Adam Gleave <adam@gleave.me>

Update sgf-viewer/src/defense/landing-page-content.ts

Co-authored-by: Adam Gleave <adam@gleave.me>

Fix streamlit_app tests

* update a9 games

* copy attack games from nas to flamingo

game_infos.csv has changed for the /nas/ucb/{kpelrine,tony} games that
are no longer available, but not for any fo the columns that are
actually used for the website

* delete 'server' content field

* Switch contadv to v65536 games

* rm unison files

* merge a bunch of content

* basic navbar

* better navbar dropdown highlighting

* organize into correct navbar format

* anchor link adjustments

* fix some links

* update homepage content

* copyediting- remove some hyphens

* postional adv training summary paragraph

* update continuous adv content

* update a9 vs v65536 games

* update gift attack description

* iterated attack header description

* validation description

* a9 description

* a9 v65536 win rate

* vit content

* fix json error

* add defense sumary fig, update citation footer to include both

go_attack_utils/src/sgf_parser/game_info.py
@@ -9,6 +9,9 @@ import multiprocessing
 import warnings
 import functools
 
+DEFAULT_ADVERSARY_SUBSTRINGS = ["adv"]
+DEFAULT_VICTIM_SUBSTRINGS = ["victim", "bot"]
+
 
 def get_game_str(path: pathlib.Path, line_num: int):
     """Return the string at a given path and line number."""
@@ -71,8 +74,15 @@ def read_and_parse_file(
     fast_parse: bool = False,
     victim_color: Optional[str] = None,
     no_victim_okay: bool = False,
+    adversary_substrings: Optional[Sequence[str]] = None,
+    victim_substrings: Optional[Sequence[str]] = None,
 ) -> Sequence[Dict[str, Any]]:
     """Parse all lines of an sgf file to a list of dictionaries with game info."""
+    if adversary_substrings is None:
+        adversary_substrings = DEFAULT_ADVERSARY_SUBSTRINGS
+    if victim_substrings is None:
+        victim_substrings = DEFAULT_VICTIM_SUBSTRINGS
+
     parsed_games = []
     with open(path, "r") as f:
         for i, line in enumerate(f):
@@ -84,6 +94,8 @@ def read_and_parse_file(
                     fast_parse=fast_parse,
                     victim_color=victim_color,
                     no_victim_okay=no_victim_okay,
+                    adversary_substrings=adversary_substrings,
+                    victim_substrings=victim_substrings,
                 )
             )
     return parsed_games
@@ -94,14 +106,23 @@ def read_and_parse_all_files(
     fast_parse: bool = False,
     processes: Optional[int] = 128,
     no_victim_okay: bool = False,
+    adversary_substrings: Optional[Sequence[str]] = None,
+    victim_substrings: Optional[Sequence[str]] = None,
 ) -> Sequence[Dict[str, Any]]:
     """Returns concatenated contents of all files in `paths`."""
+    if adversary_substrings is None:
+        adversary_substrings = DEFAULT_ADVERSARY_SUBSTRINGS
+    if victim_substrings is None:
+        victim_substrings = DEFAULT_VICTIM_SUBSTRINGS
+
     if not processes:
         processes = min(128, len(paths) // 2)
     read_and_parse_file_partial = functools.partial(
         read_and_parse_file,
         fast_parse=fast_parse,
         no_victim_okay=no_victim_okay,
+        adversary_substrings=adversary_substrings,
+        victim_substrings=victim_substrings,
     )
     with multiprocessing.Pool(processes=max(processes, 1)) as pool:
         parsed_games = pool.map(read_and_parse_file_partial, paths)
@@ -139,6 +160,8 @@ def parse_game_str_to_dict(
     fast_parse: bool = False,
     victim_color: Optional[str] = None,
     no_victim_okay: bool = False,
+    adversary_substrings: Optional[Sequence[str]] = None,
+    victim_substrings: Optional[Sequence[str]] = None,
 ) -> Dict[str, Any]:
     """Parse an sgf string to a dictionary containing game_info.
 
@@ -153,10 +176,18 @@ def parse_game_str_to_dict(
             don't label the adversary or victim).
         no_victim_okay: If True, don't raise an error if the game doesn't have
             a victim.
+        adversary_substrings: Substrings that indicate the adversary in PB or
+            PW.
+        victim_substrings: Substrings that indicate the victim in PB or PW.
 
     Returns:
         Dictionary containing game_info.
     """
+    if adversary_substrings is None:
+        adversary_substrings = DEFAULT_ADVERSARY_SUBSTRINGS
+    if victim_substrings is None:
+        victim_substrings = DEFAULT_VICTIM_SUBSTRINGS
+
     rule_str = extract_prop("RU", sgf_str)
     comment_str = extract_prop("C", sgf_str)
     board_size = extract_prop("SZ", sgf_str)
@@ -169,12 +200,18 @@ def parse_game_str_to_dict(
     komi = extract_prop("KM", sgf_str)
     komi = float(komi) if komi else komi
     win_color = result[0].lower() if result else None
-    assert no_victim_okay or (
-        "victim" in b_name.lower()
-        or "victim" in w_name.lower()
-        or "adv" in b_name.lower()
-        or "adv" in w_name.lower()
-        or victim_color
+
+    if victim_color is None:
+        b_name_has_victim = any(x in b_name.lower() for x in victim_substrings)
+        w_name_has_victim = any(x in w_name.lower() for x in victim_substrings)
+        b_name_has_adversary = any(x in b_name.lower() for x in adversary_substrings)
+        w_name_has_adversary = any(x in w_name.lower() for x in adversary_substrings)
+        victim_is_black = b_name_has_victim or w_name_has_adversary
+        victim_is_white = w_name_has_victim or b_name_has_adversary
+        if victim_is_black != victim_is_white:
+            victim_color = "b" if victim_is_black else "w"
+    assert (
+        no_victim_okay or victim_color is not None
     ), f"Game doesn't have victim: path={path}, line_number={line_number}"
 
     parts = pathlib.Path(path).parts
@@ -186,20 +223,9 @@ def parse_game_str_to_dict(
     elif "gatekeepersgf" in parts:
         training = "gating"
 
-    if victim_color is None:
-        victim_color = {b_name: "b", w_name: "w"}.get(
-            "victim",
-            "b"
-            if (
-                "victim" in b_name.lower()
-                or "bot" in b_name.lower()
-                or "adv" in w_name.lower()
-            )
-            else "w",
-        )
     victim_name = {"b": b_name, "w": w_name}[victim_color]
     adv_color = {"b": "w", "w": "b"}[victim_color]
-    adv_name = b_name if adv_color == "b" else w_name
+    adv_name = {"b": b_name, "w": w_name}[adv_color]
     if victim_name in ["bot-cp127-v1", "bot-cp505-v2", "bot-cp505-v1"]:
         victim_steps = {
             "bot-cp127-v1": 5303129600,
@@ -271,11 +297,13 @@ def parse_game_str_to_dict(
         # Victim info
         "victim_color": victim_color,
         "victim_name": victim_name,
-        "victim_visits": victim_visits
-        if victim_visits
-        else int(str(victim_rank).lstrip("v"))
-        if victim_rank
-        else 1,
+        "victim_visits": (
+            victim_visits
+            if victim_visits
+            else int(str(victim_rank).lstrip("v"))
+            if victim_rank
+            else 1
+        ),
         "victim_steps": victim_steps,
         "victim_rsym": extract_param("rsym", victim_rank),
         "victim_algo": extract_param("algo", victim_rank),

commit 43bbc23f0d36c581a29f47b84dc65cc58d35c24e

game_info: Fix no-victim game parsing
Broken by #99 --- previously when there was no victim we'd just
arbitrarily pick a victim color. PR #99 changed this to give no victim
color, which is reasonable, but other code that assumed that
victim_color is always set broke.

go_attack_utils/src/sgf_parser/game_info.py
@@ -200,6 +200,41 @@ def parse_game_str_to_dict(
     komi = extract_prop("KM", sgf_str)
     komi = float(komi) if komi else komi
     win_color = result[0].lower() if result else None
+    is_resignation = False
+    if win_color is not None:
+        win_score_str = (
+            result.split("+")[-1]
+            if "+" in result
+            # Sgfs for manual games can have a space instead of a +
+            else result.split(" ")[-1]
+        )
+        if win_score_str == "R" or win_score_str == "Resign":
+            win_score = None
+            is_resignation = True
+        else:
+            win_score = float(win_score_str)
+
+    b_meta = extract_prop("BR", sgf_str)
+    w_meta = extract_prop("WR", sgf_str)
+    b_visits = (
+        extract_re(r"v([0-9]+)", b_meta) or extract_re(r"v=([0-9]+)", b_meta) or "-1"
+        if b_meta
+        else None
+    )
+    w_visits = (
+        extract_re(r"v([0-9]+)", w_meta) or extract_re(r"v=([0-9]+)", w_meta) or "-1"
+        if w_meta
+        else None
+    )
+
+    parts = pathlib.Path(path).parts
+    training = None
+    if "eval" in parts:
+        training = "eval"
+    elif "selfplay" in parts:
+        training = "train"
+    elif "gatekeepersgf" in parts:
+        training = "gating"
 
     if victim_color is None:
         b_name_has_victim = any(x in b_name.lower() for x in victim_substrings)
@@ -214,82 +249,57 @@ def parse_game_str_to_dict(
         no_victim_okay or victim_color is not None
     ), f"Game doesn't have victim: path={path}, line_number={line_number}"
 
-    parts = pathlib.Path(path).parts
-    training = None
-    if "eval" in parts:
-        training = "eval"
-    elif "selfplay" in parts:
-        training = "train"
-    elif "gatekeepersgf" in parts:
-        training = "gating"
-
-    victim_name = {"b": b_name, "w": w_name}[victim_color]
-    adv_color = {"b": "w", "w": "b"}[victim_color]
-    adv_name = {"b": b_name, "w": w_name}[adv_color]
-    if victim_name in ["bot-cp127-v1", "bot-cp505-v2", "bot-cp505-v1"]:
-        victim_steps = {
-            "bot-cp127-v1": 5303129600,
-            "bot-cp505-v2": 11840935168,
-            "bot-cp505-v1": 11840935168,
-        }[victim_name]
-    else:
-        victim_steps = (
-            # Extract step count after "-s" in the name, allowing step count to end
-            # with "m" (for millions). After "-s<number>" we expect "-" or "."
-            # (for names like t0-s0-d0 and victim-s1m.bin.gz) or the end of the
-            # string.
-            extract_re("-s(\d+m?)(?:[-.]|$)", victim_name)
-            or extract_re("kata[^_]+?\-s([0-9]+)\-", "/".join(parts[-3:]))
+    victim_name = None
+    victim_steps = None
+    victim_rank = None
+    victim_visits = None
+    adv_color = None
+    adv_name = None
+    adv_steps = None
+    adv_rank = None
+    adv_visits = None
+    adv_komi = None
+    adv_samples = None
+    adv_minus_victim_score = None
+    adv_minus_victim_score_wo_komi = None
+    if victim_color is not None:
+        victim_name = {"b": b_name, "w": w_name}[victim_color]
+        adv_color = {"b": "w", "w": "b"}[victim_color]
+        adv_name = {"b": b_name, "w": w_name}[adv_color]
+        if victim_name in ["bot-cp127-v1", "bot-cp505-v2", "bot-cp505-v1"]:
+            victim_steps = {
+                "bot-cp127-v1": 5303129600,
+                "bot-cp505-v2": 11840935168,
+                "bot-cp505-v1": 11840935168,
+            }[victim_name]
+        else:
+            victim_steps = (
+                # Extract step count after "-s" in the name, allowing step count to end
+                # with "m" (for millions). After "-s<number>" we expect "-" or "."
+                # (for names like t0-s0-d0 and victim-s1m.bin.gz) or the end of the
+                # string.
+                extract_re("-s(\d+m?)(?:[-.]|$)", victim_name)
+                or extract_re("kata[^_]+?\-s([0-9]+)\-", "/".join(parts[-3:]))
+                or 0
+            )
+        adv_rank = (
+            extract_prop("BR", sgf_str) if adv_color == "b" else extract_prop("WR", sgf_str)
+        )
+        victim_rank = (
+            extract_prop("BR", sgf_str) if adv_color == "w" else extract_prop("WR", sgf_str)
+        )
+        victim_visits = {"b": b_visits, "w": w_visits}[victim_color]
+        adv_visits = {"b": b_visits, "w": w_visits}[adv_color]
+        adv_komi = None if adv_color is None else komi * {"w": 1, "b": -1}[adv_color]
+        adv_steps = (
+            extract_re(r"\-s([0-9]+)\-", adv_name)
+            or extract_re(r"t0\-s([0-9]+)\-", "/".join(parts[-3:]))
             or 0
         )
-    adv_rank = (
-        extract_prop("BR", sgf_str) if adv_color == "b" else extract_prop("WR", sgf_str)
-    )
-    victim_rank = (
-        extract_prop("BR", sgf_str) if adv_color == "w" else extract_prop("WR", sgf_str)
-    )
-    b_meta = extract_prop("BR", sgf_str)
-    w_meta = extract_prop("WR", sgf_str)
-    b_visits = (
-        extract_re(r"v([0-9]+)", b_meta) or extract_re(r"v=([0-9]+)", b_meta) or "-1"
-        if b_meta
-        else None
-    )
-    w_visits = (
-        extract_re(r"v([0-9]+)", w_meta) or extract_re(r"v=([0-9]+)", w_meta) or "-1"
-        if w_meta
-        else None
-    )
-    victim_visits = {"b": b_visits, "w": w_visits}[victim_color]
-    adv_visits = {"b": b_visits, "w": w_visits}[adv_color]
-    adv_komi = komi * {"w": 1, "b": -1}[adv_color]
-    is_resignation = False
-    if win_color is None:
-        adv_minus_victim_score = 0
-        adv_minus_victim_score_wo_komi = None
-    else:
-        win_score_str = (
-            result.split("+")[-1]
-            if "+" in result
-            # Sgfs for manual games can have a space instead of a +
-            else result.split(" ")[-1]
-        )
-        if win_score_str == "R" or win_score_str == "Resign":
-            # Resignation
-            win_score = None
-            adv_minus_victim_score = None
-            adv_minus_victim_score_wo_komi = None
-            is_resignation = True
-        else:
-            win_score = float(win_score_str)
+        adv_samples = extract_re(r"\-d([0-9]+)", adv_name) or 0
+        if win_score is not None:
             adv_minus_victim_score = win_score if adv_color == win_color else -win_score
             adv_minus_victim_score_wo_komi = adv_minus_victim_score - adv_komi
-    adv_steps = (
-        extract_re(r"\-s([0-9]+)\-", adv_name)
-        or extract_re(r"t0\-s([0-9]+)\-", "/".join(parts[-3:]))
-        or 0
-    )
-    adv_samples = extract_re(r"\-d([0-9]+)", adv_name) or 0
 
     parsed_info = {
         "b_name": b_name,